d81b4d116b36171f2c8d208c514e4289646162eb
Devin Rodriguez <devin@Devins-MBP.lan>
2023-06-05 20:01:31 -0400

    True Working

--- Controllers/foodEntryController.js
+++ Controllers/foodEntryController.js
@@ -1,132 +1,3 @@
-// const axios = require("axios");
-// const express = require("express");
-// require ("dotenv").config();
-// const FoodEntry = require("../Models/foodEntryModel");
-// const User = require("../Models/userModel");
-
-// // USDA API
-// const API_KEY = "Dlit4ChVaFEMTFauFNHK1FEHVM7hAtTulyA7h26E";
-
-// // Get all food entries for a user
-// exports.getAllEntries = async (req, res) => {
-//     try {
-//         const user = await User.findById(req.params.userId);
-//         if (!user) {
-//             return res.status(404).json({ message: "User not found" });
-//         }
-
-//         const entries = await FoodEntry.find({ user: user._id });
-//         res.status(200).json(entries);
-//     } catch (err) {
-//         res.status(400).json({ message: err.message });
-//     }
-// };
-
-// // Get a specific food entry for a user
-// exports.getEntry = async (req, res) => {
-//     try {
-//         const entry = await FoodEntry.findOne({ _id: req.params.entryId, user: req.params.userId });
-//         if (!entry) {
-//             return res.status(404).json({ message: "Entry not found" });
-//         }
-//         res.status(200).json(entry);
-//     } catch (err) {
-//         res.status(400).json({ message: err.message });
-//     }
-// };
-
-// // Post a new food entry for a user
-// exports.createEntry = async (req, res) => {
-//     try {
-//         const { foodName, foodItemId, userId } = req.body;
-//         const response = await axios.get(
-//             `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&query=${foodName}`
-//         );
-
-//         const food = response.data.foods.find((f) => f.fdcId == foodItemId);
-//         if (!food) {
-//             return res.status(404).json({ message: "Food not found" });
-//         }
-
-//         const nutrients = food.foodNutrients;
-//         const newEntry = new FoodEntry({
-//             user: userId,
-//             foodName: foodName,
-//             foodItemId: foodItemId,
-//             calories: nutrients.find((n) => n.nutrientName === "Energy")?.value,
-//             carbs: nutrients.find((n) => n.nutrientName === "Carbohydrate, by difference")?.value,
-//             protein: nutrients.find((n) => n.nutrientName === "Protein")?.value,
-//             fats: nutrients.find((n) => n.nutrientName === "Total lipid (fat)")?.value,
-//             timestamp: Date.now(),
-//         });
-
-//         const savedEntry = await newEntry.save();
-//         res.status(201).json(savedEntry);
-//     } catch (err) {
-//         res.status(400).json({ message: err.message });
-//     }
-// };
-
-// // Update a food entry for a user
-// // Update a food entry for a user
-// exports.updateEntry = async (req, res) => {
-//   try {
-//       const { foodName, foodItemId } = req.body;
-
-//       let entry = await FoodEntry.findOne({ _id: req.params.entryId, user: req.params.userId });
-//       if (!entry) {
-//           return res.status(404).json({ message: "Entry not found" });
-//       }
-
-//       const response = await axios.get(
-//           `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&query=${foodName}`
-//       );
-
-//       const food = response.data.foods.find((f) => f.fdcId == foodItemId);
-//       if (!food) {
-//           return res.status(404).json({ message: "Food not found" });
-//       }
-
-//       const nutrients = food.foodNutrients;
-
-//       entry.foodName = foodName;
-//       entry.foodItemId = foodItemId;
-//       entry.calories = nutrients.find((n) => n.nutrientName === "Energy")?.value;
-//       entry.carbs = nutrients.find((n) => n.nutrientName === "Carbohydrate, by difference")?.value;
-//       entry.protein = nutrients.find((n) => n.nutrientName === "Protein")?.value;
-//       entry.fats = nutrients.find((n) => n.nutrientName === "Total lipid (fat)")?.value;
-//       entry.timestamp = Date.now();
-
-//       const updatedEntry = await entry.save();
-//       res.status(200).json(updatedEntry);
-//   } catch (err) {
-//       res.status(400).json({ message: err.message });
-//   }
-// };
-
-
-// // Delete a food entry for a user
-// exports.deleteEntry = async (req, res) => {
-//     try {
-//         let entry = await FoodEntry.findOne({ _id: req.params.entryId, user: req.params.userId });
-//         if (!entry) {
-//             return res.status(404).json({ message: "Entry not found" });
-//         }
-
-//         await FoodEntry.deleteOne({ _id: req.params.entryId });
-//         res.status(200).json({ message: "Entry deleted successfully" });
-//     } catch (err) {
-//         res.status(400).json({ message: err.message });
-//     }
-// };
-
-
-
-
-
-
-
-
 
 const axios = require("axios");
 const express = require("express");
@@ -141,6 +12,7 @@ exports.createEntry = async (req, res) => {
   console.log(req.body);
   try {
     const { foodName, foodItemId, user } = req.body;
+    console.log(foodName);
     const response = await axios.get(
       `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&query=${foodName}`
     );
@@ -153,7 +25,7 @@ exports.createEntry = async (req, res) => {
 
     console.log(response.data);
 
-    const nutrients = food.foodNutrients;
+    const nutrients = response.data.foods[0].foodNutrients;
     const newEntry = new FoodEntry({
       user: user,
       foodName: foodName,
